0de8f33220a9def9403743163247370b537ed2ce
Mihai Nicoara <nicoaram@s.dcsdk12.org>
2023-01-14 20:13:06 -0700

    changed sysout to shuffleboard

--- Navx2TestBoard/src/main/java/frc/robot/Robot.java
+++ Navx2TestBoard/src/main/java/frc/robot/Robot.java
@@ -12,26 +12,31 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import com.kauailabs.navx.frc.AHRS;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  
+
   private int stallCounter = 0;
   public AHRS ahrs = new AHRS();
 
   private RobotContainer m_robotContainer;
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
     ahrs.calibrate();
@@ -39,29 +44,40 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
@@ -75,7 +91,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    
+
   }
 
   @Override
@@ -87,25 +103,18 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
 
-      //ahrs.calibrate();
+      // ahrs.calibrate();
     }
-    
-
-    
-
 
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-      System.out.println("The current angle is: " + ahrs.getAngle());
-      System.out.println("The current altitude is: " + ahrs.getAltitude());
-      System.out.println("The current compass heading is: " + ahrs.getCompassHeading());
-      System.out.println("The current pitch is: " + ahrs.getPitch());
-
-      SmartDashboard.putNumber("navX Angle",(double) ahrs.getAngle());
-      SmartDashboard.putNumber("navX Altitude",(double) ahrs.getAltitude());
+    SmartDashboard.putNumber("navX Angle", (double) ahrs.getAngle());
+    SmartDashboard.putNumber("navX Altitude", (double) ahrs.getAltitude());
+    SmartDashboard.putNumber("navX Heading", (double) ahrs.getCompassHeading());
+    SmartDashboard.putNumber("navX Pitch", (double) ahrs.getPitch());
   }
 
   @Override
@@ -116,13 +125,16 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 }
