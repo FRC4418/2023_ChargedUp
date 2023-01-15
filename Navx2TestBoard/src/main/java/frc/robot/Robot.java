28adbab437049e2163a229406b9cd6bf861b4242
Mihai Nicoara <nicoaram@s.dcsdk12.org>
2023-01-14 19:59:19 -0700

    added shuffleboard

--- Navx2TestBoard/src/main/java/frc/robot/Robot.java
+++ Navx2TestBoard/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import com.kauailabs.navx.frc.AHRS;
@@ -98,10 +99,13 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-      System.out.print("The current angle is: " + ahrs.getAngle());
-      System.out.print("The current altitude is: " + ahrs.getAltitude());
-      System.out.print("The current compass heading is: " + ahrs.getCompassHeading());
-      System.out.print("The current pitch is: " + ahrs.getPitch());
+      System.out.println("The current angle is: " + ahrs.getAngle());
+      System.out.println("The current altitude is: " + ahrs.getAltitude());
+      System.out.println("The current compass heading is: " + ahrs.getCompassHeading());
+      System.out.println("The current pitch is: " + ahrs.getPitch());
+
+      SmartDashboard.putNumber("navX Angle",(double) ahrs.getAngle());
+      SmartDashboard.putNumber("navX Altitude",(double) ahrs.getAltitude());
   }
 
   @Override
